53bc1fa42d0ac886dc76adb6a2b5fdefe62965f2
Phillip <phil.winston2@gmail.com>
2017-06-29 13:55:23 -0400

    add google font, update styles, add button over video, restructure files

--- js/main.js
+++ js/main.js
@@ -2,8 +2,44 @@ $(document).ready(function() {
   var $header = $("header"),
       $clone = $header.before($header.clone().addClass("clone").removeClass("mini"));
 
-       $(window).on("scroll", function() {
-           var fromTop = $(window).scrollTop();
-           $("body").toggleClass("down", (fromTop > 810));
-       });
+  // add sticky nav after scrolling down
+  $(window).on("scroll", function() {
+    var fromTop = $(window).scrollTop();
+    $("body").toggleClass("down", (fromTop > 810));
+  });
+
+  $("#intro-video").click(function(){
+    removeIntroButtons();
+  });
+
+  // watch intro video
+  // $(".watch").click(function(e) {
+  //   e.preventDefault();
+  //   removeIntroButtons();
+  //   var videoSrc = $("#intro-video").attr('src');
+  //   // start autoplay
+  //   $("#intro-video").attr('src', videoSrc  + "&autoplay=1");
+  // });
+
+  // scroll down to main section
+  $(".continue").click(function(e) {
+    e.preventDefault();
+    var videoSrc = $("#intro-video").attr('src');
+    // stop video
+    $("#intro-video").attr('src', videoSrc);
+    setTimeout(function() {
+    $('html, body').animate({
+    scrollTop: $("#main-content").offset().top
+    }, 1000);
+    }, 100);
+
+    setTimeout(function() {
+      removeIntroButtons();
+    }, 1000);
+  });
+
+  function removeIntroButtons() {
+    $(".intro-button").remove();
+  }
+
 });
